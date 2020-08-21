93bf38f080422b7d28384e53b34fe50c3c0ecde5
christopherjurthe <57255003+christopherjurthe@users.noreply.github.com>
2020-08-21 13:52:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
                 <Header></Header>
             </div>
             <div className="main">
-
                 <Hooks />
                 <Band />
             </div>
