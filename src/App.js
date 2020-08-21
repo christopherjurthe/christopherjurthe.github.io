9a6bfb8d5984f3e5a9255baf1ddf4a0489cece61
christopherjurthe <57255003+christopherjurthe@users.noreply.github.com>
2020-08-21 11:51:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
             </div>
             <div className="main">
 
-                {/* <Hooks /> */}
-                <Band />
+                <Hooks />
+                {/* <Band /> */}
             </div>
             <div className="footer">
                 <Footer></Footer>
